94f381ba7eda0770a1a12ee086eb2652061d514d
Deivison Cardoso <deivisondc@gmail.com>
2023-01-22 11:30:44 -0300

    add transitions and outline styles

--- src/components/HabitsList.tsx
+++ src/components/HabitsList.tsx
@@ -59,12 +59,15 @@ export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
         {habitsInfo?.possibleHabits.map(habit => (
           <Checkbox.Root
             key={habit.id}
-            className='flex items-center gap-3 group'
+            className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed'
             checked={habitsInfo.completedHabits.includes(habit.id)}
             onCheckedChange={() => handleToggleHabit(habit.id)}
             disabled={isDateIsPast}
           >
-            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 transition-colors 
+              group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 
+              group-focus:outline-none group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'
+            >
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
